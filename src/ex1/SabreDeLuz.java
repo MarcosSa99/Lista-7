d99d117e35bbb4473057253d88f32d3c8001550a
Marcos Sá <marcos.sa@polijunior.com.br>
2018-05-24 03:05:22 -0300

    Sabre de Luz

--- src/ex1/SabreDeLuz.java
+++ src/ex1/SabreDeLuz.java
@@ -1,5 +1,22 @@
 package ex1;
 
 public class SabreDeLuz {
-
+	private String Cor;
+	private String Punho;
+	public SabreDeLuz (String Cor, String Punho){
+		this.Cor = Cor;
+		this.Punho = Punho;
+	}
+	public String getCor() {
+		return Cor;
+	}
+	public void setCor(String cor) {
+		Cor = cor;
+	}
+	public String getPunho() {
+		return Punho;
+	}
+	public void setPunho(String punho) {
+		Punho = punho;
+	}
 }
