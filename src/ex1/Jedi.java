5e620aa9f168a40efd7fec0fd3ccb91ed2d2c3f1
Marcos Sá <marcos.sa@polijunior.com.br>
2018-05-24 02:08:40 -0300

    Classe jedi

--- src/ex1/Jedi.java
+++ src/ex1/Jedi.java
@@ -1,10 +1,48 @@
 package ex1;
 
 public abstract class Jedi extends SerVivo {
-	public Jedi(boolean Genêro, String Espécie, String DataNascimento, boolean Alive, String Nome, String PlanetaNatal) {
-		super(Genêro, Espécie, DataNascimento, Alive, Nome, PlanetaNatal);
-		
-		
+	private double PazInterna;
+	private boolean Telepatia;
+	private boolean Telecinese;
+	private boolean Persuasão;
+	private double CampoDeVisão;
+	public Jedi(boolean Genêro, String Espécie, String DataNascimento, boolean Alive, String Nome, String PlanetaNatal, int MidChlorians, double PazInterna, boolean Telepatia, boolean Telecinese, boolean Persuasão, double CampoDeVisão) {
+		super(Genêro, Espécie, DataNascimento, Alive, Nome, PlanetaNatal, MidChlorians);
+		this.PazInterna = PazInterna;
+		this.Telepatia = Telepatia;
+		this.Telecinese = Telecinese;
+		this.Persuasão = Persuasão;
+		this.CampoDeVisão = CampoDeVisão;
+	}
+	public double getPazInterna() {
+		return PazInterna;
+	}
+	public void setPazInterna(double pazInterna) {
+		PazInterna = pazInterna;
+	}
+	public boolean isTelepatia() {
+		return Telepatia;
+	}
+	public void setTelepatia(boolean telepatia) {
+		Telepatia = telepatia;
+	}
+	public boolean isTelecinese() {
+		return Telecinese;
+	}
+	public void setTelecinese(boolean telecinese) {
+		Telecinese = telecinese;
+	}
+	public boolean isPersuasão() {
+		return Persuasão;
+	}
+	public void setPersuasão(boolean persuasão) {
+		Persuasão = persuasão;
+	}
+	public double getCampoDeVisão() {
+		return CampoDeVisão;
+	}
+	public void setCampoDeVisão(double campoDeVisão) {
+		CampoDeVisão = campoDeVisão;
 	}
 
 }
